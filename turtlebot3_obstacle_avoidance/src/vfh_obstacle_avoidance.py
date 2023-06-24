4e2d5313ea7d4e705da6e9bb55ee5d9a3b4265bb
aliasad059 <aliasad059@gmail.com>
2023-06-24 16:09:36 -0400

    feat: method to find candidate valleys and set streering direction

--- turtlebot3_obstacle_avoidance/src/vfh_obstacle_avoidance.py
+++ turtlebot3_obstacle_avoidance/src/vfh_obstacle_avoidance.py
@@ -5,7 +5,7 @@ from sensor_msgs.msg import LaserScan
 from scipy.signal import argrelmin
 import numpy as np
 from turtlebot3_obstacle_avoidance.srv import ObstacleAvoidanceService, ObstacleAvoidanceServiceResponse
-
+import math
 
 class VFH():
     def __init__(self, vfh_config) -> None:
@@ -24,7 +24,7 @@ class VFH():
     
     def obstacle_avoidance_callback(self):
         response = ObstacleAvoidanceServiceResponse()
-        response.goal_angle = self.find_best_direction()
+        response.goal_angle = self.find_steering_direction()
         return response
 
     def scan_callback(self, msg):
@@ -61,7 +61,7 @@ class VFH():
 
         return smoothed_histogram
     
-    def calculate_goal_sector(self):
+    def get_goal_sector(self):
         current_position = np.array([self.current_x, self.current_y])
         goal_direction = np.array([self.destination[0] - current_position[0], self.destination[1] - current_position[1]])
         goal_angle = np.arctan2(goal_direction[1], goal_direction[0])
@@ -71,22 +71,65 @@ class VFH():
         goal_sector = int(goal_angle_deg / self.sector_size)
         
         return goal_sector
-
-    def find_best_direction(self):
-        troughs = argrelmin(self.histogram_field_vector)[0]
-        
-        if len(troughs) > 0:
-            eligible_troughs = [trough for trough in troughs if self.histogram_field_vector[trough] < self.threshold]
+    
+    def get_candidate_valleys(self):
+        valleys_idx = []
+        for i in range(len(self.histogram_field_vector)):
+            if self.histogram_field_vector[i] < self.VFH_THRESHOLD:
+                valleys_idx.append(i)
             
-            if len(eligible_troughs) > 0:
-                goal_sector = self.calculate_goal_sector()
-                if goal_sector in eligible_troughs:
-                    return goal_sector
-                else:
-                    return min(eligible_troughs, key=lambda t: abs(t - self.destination)) # TODO: here we should find the nearest vally to goal and select the suitable sector
+        valleys = []
+        candidate_valley = []
+
+        i = 0
+        while i < len(valleys_idx):
+            while i < len(valleys_idx) and valleys_idx[i] == (valleys_idx[i-1])%len(self.histogram_field_vector) + 1:
+                candidate_valley.append(valleys_idx[i])
+                i += 1
+            
+            if len(candidate_valley) > 0:
+                valleys.append(deepcopy(candidate_valley))
+                candidate_valley.clear()
+                if i >= len(valleys_idx):
+                    break
+            candidate_valley.append(valleys_idx[i])
+            i += 1
         
-        # If no eligible troughs, choose the peak as the direction
-        return np.argmax(self.histogram_field_vector)
+        if len(valleys) >= 2:
+            val_begin = valleys[0]
+            val_end = valleys[len(valleys)-1]
+            if (val_end[len(val_end)-1] + 1)%len(self.histogram_field_vector) == val_begin[0]:
+                val_begin = val_end + val_begin  
+                valleys[0] = val_begin
+                valleys.pop(len(valleys)-1)
+        return valleys
+    
+    def find_steering_direction(self):
+        valleys = self.get_candidate_valleys()
+        
+        goal_sector = self.get_goal_sector()
+        
+        min_distance = np.inf
+        nearest_valley = None
+        kn = None
+        for candidate_valley in valleys:
+            if goal_sector in candidate_valley:
+                return math.radians(goal_sector * self.sector_size) # goal sector is in a candidate valley
+            
+            distances = np.abs(np.array(candidate_valley) - goal_sector)
+            min_idx = np.argmin(distances)
+            if distances[min_idx] < min_distance:
+                min_distance = distances[min_idx]
+                nearest_valley = candidate_valley
+                kn = candidate_valley[min_idx]
+        
+        if len(nearest_valley) > self.VFH_S_MAX: # wide candidate valley
+            kf = (kn + self.VFH_S_MAX) % (360/5)
+            teta = (kn+kf)/2 * self.sector_size
+        else: # narrow candidate valley
+            teta = (nearest_valley[0] + nearest_valley[-1]) / 2 * self.sector_size
+
+        return math.radians(teta % 360)
     
     def run(self):
         rospy.spin()
